5ca76d0ea3844c1a431c3f1669788ca351dd303c
patrick <xcpxcp198608@163.com>
2017-01-06 10:53:54 +0800

    201701061053

--- app/src/main/java/com/wiatec/btv_launcher/fragment/Fragment1.java
+++ app/src/main/java/com/wiatec/btv_launcher/fragment/Fragment1.java
@@ -6,42 +6,36 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.jude.rollviewpager.RollPagerView;
+import com.wiatec.btv_launcher.Activity.MenuActivity;
+import com.wiatec.btv_launcher.Activity.MessageActivity;
 import com.wiatec.btv_launcher.Activity.OpportunityActivity;
+import com.wiatec.btv_launcher.Activity.PlayActivity;
 import com.wiatec.btv_launcher.Activity.PlayAdActivity;
 import com.wiatec.btv_launcher.Activity.UserManualActivity;
 import com.wiatec.btv_launcher.Application;
 import com.wiatec.btv_launcher.F;
 import com.wiatec.btv_launcher.OnNetworkStatusListener;
+import com.wiatec.btv_launcher.R;
 import com.wiatec.btv_launcher.Utils.ApkCheck;
 import com.wiatec.btv_launcher.Utils.ApkLaunch;
 import com.wiatec.btv_launcher.Utils.Logger;
-import com.wiatec.btv_launcher.Activity.MenuActivity;
-import com.wiatec.btv_launcher.R;
-import com.wiatec.btv_launcher.Activity.MessageActivity;
-import com.wiatec.btv_launcher.Activity.UserGuideActivity;
 import com.wiatec.btv_launcher.Utils.SystemConfig;
 import com.wiatec.btv_launcher.adapter.RollImageAdapter;
 import com.wiatec.btv_launcher.animator.Zoom;
-import com.wiatec.btv_launcher.bean.CloudImageInfo;
 import com.wiatec.btv_launcher.bean.ImageInfo;
-import com.wiatec.btv_launcher.bean.RollImageInfo;
 import com.wiatec.btv_launcher.bean.VideoInfo;
 import com.wiatec.btv_launcher.presenter.Fragment1Presenter;
 import com.wiatec.btv_launcher.receiver.NetworkStatusReceiver;
-import com.wiatec.btv_launcher.service.LoadCloudService;
 
 import java.io.File;
 import java.util.List;
@@ -53,23 +47,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.Observable;
-import rx.Scheduler;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
-import rx.functions.Func0;
 import rx.functions.Func1;
-import rx.functions.Func2;
 import rx.schedulers.Schedulers;
-
-import static rx.Observable.just;
 
 /**
  * Created by PX on 2016-11-12.
  */
 
-public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> implements IFragment1 ,OnNetworkStatusListener,View.OnFocusChangeListener {
+public class Fragment1 extends BaseFragment<IFragment1, Fragment1Presenter> implements IFragment1, OnNetworkStatusListener, View.OnFocusChangeListener {
     @BindView(R.id.ibt_btv)
     ImageButton ibt_Btv;
     @BindView(R.id.ibt_user_guide)
@@ -94,58 +83,54 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
     VideoView vv_Main;
     @BindView(R.id.ibt_ld_cloud)
     ImageButton ibt_LdCloud;
+    @BindView(R.id.fl_video)
+    FrameLayout flVideo;
 
     private boolean isF1Visible = false;
-    private int playPosition=0;
+    private int playPosition = 0;
     private NetworkStatusReceiver networkStatusReceiver;
     private Subscription subscription;
     private Subscription videoSubscription;
-    private boolean videoStart = false;
-    private ExecutorService executorService;
+    private VideoInfo currentVideoInfo;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       // Logger.d("f1 -onCreateView ");
+        // Logger.d("f1 -onCreateView ");
         View view = inflater.inflate(R.layout.fragment1, container, false);
         ButterKnife.bind(this, view);
         networkStatusReceiver = new NetworkStatusReceiver(null);
         networkStatusReceiver.setOnNetworkStatusListener(this);
         getContext().registerReceiver(networkStatusReceiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
-        executorService = Executors.newCachedThreadPool();
         return view;
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        if(isVisibleToUser){
+        if (isVisibleToUser) {
             isF1Visible = true;
             //Logger.d("f1 -isVisibleToUser " + isVisibleToUser);
-            if(vv_Main!=null && !vv_Main.isPlaying() && isF1Visible){
-               // Logger.d("f1 -isVisibleToUser " +"play");
+            if (vv_Main != null && !vv_Main.isPlaying() && isF1Visible) {
+                // Logger.d("f1 -isVisibleToUser " +"play");
                 //playVideo();
-                if(SystemConfig.isNetworkConnected(Application.getContext())){
+                if (SystemConfig.isNetworkConnected(Application.getContext())) {
                     presenter.loadVideo();
                 }
             }
-        }else {
-           // Logger.d("f1 -isVisibleToUser " + isVisibleToUser);
-            isF1Visible =false;
-            if(vv_Main!=null){
+        } else {
+            // Logger.d("f1 -isVisibleToUser " + isVisibleToUser);
+            isF1Visible = false;
+            if (vv_Main != null) {
                 //Logger.d("f1 -stopPlayback " + "f1-->" +isF1Visible);
                 playPosition = vv_Main.getCurrentPosition();
                 vv_Main.stopPlayback();
             }
-            if(subscription!= null){
+            if (subscription != null) {
                 subscription.unsubscribe();
             }
-            if(videoSubscription!= null) {
+            if (videoSubscription != null) {
                 videoSubscription.unsubscribe();
-            }
-            videoStart = false;
-            if(executorService!= null){
-                executorService.shutdownNow();
             }
         }
     }
@@ -166,16 +151,16 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
     public void onResume() {
         super.onResume();
         //Logger.d("f1 -onResume ");
-        if(vv_Main!=null && !vv_Main.isPlaying()){
-            if(isF1Visible) {
+        if (vv_Main != null && !vv_Main.isPlaying()) {
+            if (isF1Visible) {
                 //Logger.d("f1 -onResume " +"play");
                 //playVideo();
-                if(SystemConfig.isNetworkConnected(Application.getContext())){
+                if (SystemConfig.isNetworkConnected(Application.getContext())) {
                     presenter.loadVideo();
                 }
             }
         }
-        if(SystemConfig.isNetworkConnected(getContext())){
+        if (SystemConfig.isNetworkConnected(getContext())) {
             presenter.loadData();
         }
         presenter.loadCloudData();
@@ -185,36 +170,32 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
     public void onPause() {
         super.onPause();
         //Logger.d("f1 -onPause ");
-        if(vv_Main!=null && vv_Main.isPlaying()){
+        if (vv_Main != null && vv_Main.isPlaying()) {
             playPosition = vv_Main.getCurrentPosition();
             vv_Main.stopPlayback();
         }
-        if(subscription!= null) {
+        if (subscription != null) {
             subscription.unsubscribe();
         }
-        if(videoSubscription!= null) {
+        if (videoSubscription != null) {
             videoSubscription.unsubscribe();
-        }
-        videoStart = false;
-        if(executorService!= null){
-            executorService.shutdownNow();
         }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        videoStart = false;
         //Logger.d("f1 -onDestroyView ");
         getContext().unregisterReceiver(networkStatusReceiver);
     }
 
-    @OnClick({R.id.ibt_btv, R.id.ibt_user_guide, R.id.ibt_setting, R.id.ibt_apps, R.id.ibt_market, R.id.ibt_anti_virus, R.id.ibt_privacy,  R.id.ibt_ld_cloud})
+    @OnClick({R.id.ibt_btv, R.id.ibt_user_guide, R.id.ibt_setting, R.id.ibt_apps, R.id.ibt_market,
+            R.id.ibt_anti_virus, R.id.ibt_privacy, R.id.ibt_ld_cloud ,R.id.fl_video})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.ibt_btv:
-                Intent intent = new Intent(getContext() , PlayAdActivity.class);
-                intent.putExtra("packageName" , F.package_name.btv);
+                Intent intent = new Intent(getContext(), PlayAdActivity.class);
+                intent.putExtra("packageName", F.package_name.btv);
                 startActivity(intent);
                 break;
             case R.id.ibt_user_guide:
@@ -234,26 +215,33 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
                 }
                 break;
             case R.id.ibt_anti_virus:
-                startActivity(new Intent(getContext() , OpportunityActivity.class));
+                startActivity(new Intent(getContext(), OpportunityActivity.class));
                 break;
             case R.id.ibt_privacy:
-                startActivity(new Intent(getContext() , MessageActivity.class));
+                startActivity(new Intent(getContext(), MessageActivity.class));
                 break;
             case R.id.ibt_ld_cloud:
                 if (ApkCheck.isApkInstalled(getContext(), F.package_name.cloud)) {
                     ApkLaunch.launchApkByPackageName(getContext(), F.package_name.cloud);
                 }
                 break;
+            case R.id.fl_video:
+                if(currentVideoInfo != null){
+                    Intent intent1 = new Intent(getContext() , PlayActivity.class);
+                    intent1.putExtra("url" , currentVideoInfo.getUrl());
+                    startActivity(intent1);
+                }
+                break;
         }
     }
 
-    private void playVideo (){
+    private void playVideo() {
         vv_Main.setVideoPath(F.path.video);
         vv_Main.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 //Logger.d("f1-->prepare");
-                if(isF1Visible) {
+                if (isF1Visible) {
                     vv_Main.seekTo(playPosition);
                     vv_Main.start();
                 }
@@ -274,7 +262,7 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
                     playPosition = 0;
                     vv_Main.setVideoPath(F.path.video);
                     vv_Main.start();
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                     Logger.d(e.getMessage());
                 }
@@ -318,20 +306,20 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
 
     @Override
     public void loadCloudImage(final File[] files) {
-        if(files != null && files.length>0){
+        if (files != null && files.length > 0) {
             subscription = Observable.interval(6, TimeUnit.SECONDS).take(files.length)
                     .repeat()
                     .map(new Func1<Long, String>() {
                         @Override
                         public String call(Long aLong) {
-                            return files[Integer.parseInt(aLong+"")].getAbsolutePath();
+                            return files[Integer.parseInt(aLong + "")].getAbsolutePath();
                         }
                     })
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(new Action1<String>() {
                         @Override
                         public void call(String s) {
-                          //  Logger.d("f1--->" +s);
+                            //  Logger.d("f1--->" +s);
                             Glide.with(getContext()).load(s)
                                     .placeholder(R.drawable.ld_cloud_icon_3)
                                     .error(R.drawable.ld_cloud_icon_3)
@@ -343,78 +331,51 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
 
     @Override
     public void loadVideo(final List<VideoInfo> list) {
-        if(list != null){
-            videoStart = true;
-            if(executorService == null || executorService.isShutdown()){
-                executorService = Executors.newCachedThreadPool();
-            }
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    while(videoStart) {
-                        for (int i = 0; i < list.size(); i++) {
-                            final VideoInfo videoInfo = list.get(i);
-                            try {
-                                Thread.sleep(videoInfo.getPlayInterval());
-                                Logger.d(videoInfo.toString());
-                                getActivity().runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        if(vv_Main != null){
-                                            playVideo1(videoInfo.getUrl());
-                                        }
-                                    }
-                                });
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
+        if (list != null) {
+            videoSubscription = Observable.interval(0,list.get(0).getPlayInterval(),TimeUnit.MILLISECONDS).take(list.size())
+                    .subscribeOn(Schedulers.io())
+                    .repeat()
+                    .map(new Func1<Long, String>() {
+                        @Override
+                        public String call(Long aLong) {
+                            int i = aLong.intValue();
+                            return list.get(i).getUrl();
+                        }
+                    })
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Subscriber<String>() {
+                        @Override
+                        public void onCompleted() {
+
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+
+                        }
+
+                        @Override
+                        public void onNext(String s) {
+                            if(vv_Main != null ){
+                                if(currentVideoInfo == null){
+                                    currentVideoInfo = new VideoInfo();
+                                }
+                                currentVideoInfo.setUrl(s);
+                                playVideo1(s);
                             }
                         }
-                    }
-                }
-            });
-
-
-
-//            videoSubscription = Observable.interval(0,60,TimeUnit.SECONDS).take(list.size())
-//                    .subscribeOn(Schedulers.io())
-//                    .repeat()
-//                    .map(new Func1<Long, String>() {
-//                        @Override
-//                        public String call(Long aLong) {
-//                            int i = aLong.intValue();
-//                            return list.get(i).getUrl();
-//                        }
-//                    })
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe(new Subscriber<String>() {
-//                        @Override
-//                        public void onCompleted() {
-//
-//                        }
-//
-//                        @Override
-//                        public void onError(Throwable e) {
-//
-//                        }
-//
-//                        @Override
-//                        public void onNext(String s) {
-//                            if(vv_Main != null ){
-//                                playVideo1(s);
-//                            }
-//                        }
-//                    });
+                    });
         }
 
     }
 
-    private void playVideo1(final String url){
+    private void playVideo1(final String url) {
         vv_Main.setVideoPath(url);
         vv_Main.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 //Logger.d("f1-->prepare");
-                if(isF1Visible) {
+                if (isF1Visible) {
                     vv_Main.start();
                 }
             }
@@ -431,7 +392,7 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
                 try {
                     vv_Main.setVideoPath(url);
                     vv_Main.start();
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                     Logger.d(e.getMessage());
                 }
@@ -439,10 +400,10 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
         });
     }
 
-    public void showLinkByBrowser(String url){
+    public void showLinkByBrowser(String url) {
         try {
-            startActivity(new Intent(Intent.ACTION_VIEW , Uri.parse(url)));
-        }catch (Exception e){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        } catch (Exception e) {
             e.printStackTrace();
             Logger.d(e.getMessage());
         }
@@ -450,20 +411,20 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
 
     @Override
     public void onConnected(boolean isConnected) {
-        if(isConnected){
+        if (isConnected) {
             presenter.loadData();
-           // presenter.loadVideo();
+            presenter.loadVideo();
         }
     }
 
     @Override
     public void onDisconnect(boolean disConnected) {
-        if(disConnected){
-            Toast.makeText(getContext() ,getString(R.string.network_disconnect) ,Toast.LENGTH_LONG).show();
+        if (disConnected) {
+            Toast.makeText(getContext(), getString(R.string.network_disconnect), Toast.LENGTH_LONG).show();
         }
     }
 
-    private void setZoom (){
+    private void setZoom() {
         ibt_Btv.setOnFocusChangeListener(this);
         ibt_UserGuide.setOnFocusChangeListener(this);
         ibt_Setting.setOnFocusChangeListener(this);
@@ -478,7 +439,7 @@ public class Fragment1 extends BaseFragment<IFragment1 ,Fragment1Presenter> impl
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
-        if(hasFocus){
+        if (hasFocus) {
             Zoom.zoomIn09_10(v);
         }
     }
