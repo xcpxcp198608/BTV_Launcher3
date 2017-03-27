ef837d45614d9c15b52384e3e4248314f75c6c61
patrick <xcpxcp198608@163.com>
2017-03-27 00:07:31 -0700

    201703270007

--- app/src/main/java/com/wiatec/btv_launcher/Application.java
+++ app/src/main/java/com/wiatec/btv_launcher/Application.java
@@ -1,33 +1,14 @@
 package com.wiatec.btv_launcher;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
-import android.support.v7.app.AlertDialog;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.wiatec.btv_launcher.Activity.LoginActivity;
 import com.wiatec.btv_launcher.Utils.Logger;
-import com.wiatec.btv_launcher.Utils.RxBus;
 import com.wiatec.btv_launcher.exception.CrashHandler;
-import com.wiatec.btv_launcher.rxevent.CheckLoginEvent;
 import com.wiatec.btv_launcher.service.CheckLoginService;
-import com.wiatec.btv_launcher.service.LoadCloudService;
-import com.wiatec.btv_launcher.service.LoadWeatherService;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
 
 /**
  * Created by PX on 2016-11-14.
